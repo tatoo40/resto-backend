5dba71db42b44f7528414af0fbb117f42bde0595
tatoo40 <jfernandezberrutti@gmail.com>
2023-09-27 22:51:12 -0300

    db seddasdaa  commit

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.MYSQLDATABASE , process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
   host: process.env.MYSQLHOST,
   dialect: 'mysql',
-  port: process.env.MYSQLPORT
+  port: '25060'
   });
 
 
