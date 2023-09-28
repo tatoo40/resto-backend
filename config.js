0a22188161158f4352392ed902b57c8e1b71021e
tatoo40 <jfernandezberrutti@gmail.com>
2023-09-27 22:39:13 -0300

    db segundosdsdasda  commit

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  dialect: 'mysql', // u otro dialecto, dependiendo de tu base de datos
-  username: 'doadmin',
-  password: 'AVNS_nBVS3bB_iMfsbgiV9vU',
-  host: 'db-mysql-nyc3-75302-do-user-14322278-0.b.db.ondigitalocean.com', // o la dirección de tu servidor de base de datos
-  database: 'ordenesDb',
+  dialect: process.env.MYSQLDIALECT, // u otro dialecto, dependiendo de tu base de datos
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQLHOST, // o la dirección de tu servidor de base de datos
+  database: process.env.MYSQLDATABASE,
 
 });
 
 module.exports = sequelize;
+
+
+
 
