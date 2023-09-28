438ddfad4c45ebb698363764dadf5e42d59c00ff
tatoo40 <jfernandezberrutti@gmail.com>
2023-09-27 21:11:14 -0300

    db segundo  commit

--- config.js
+++ config.js
@@ -2,10 +2,11 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql', // u otro dialecto, dependiendo de tu base de datos
-  username: 'root',
-  password: 'Gardel2615',
-  host: 'localhost', // o la dirección de tu servidor de base de datos
+  username: 'doadmin',
+  password: 'AVNS_nBVS3bB_iMfsbgiV9vU',
+  host: 'db-mysql-nyc3-75302-do-user-14322278-0.b.db.ondigitalocean.comhost', // o la dirección de tu servidor de base de datos
   database: 'ordenesDb',
+
 });
 
 module.exports = sequelize;
